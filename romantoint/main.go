a5984272f5f311ef87894c23ff43f8ad948dfc41
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-01-18 09:32:02 +0530

    romantoint

--- romantoint/main.go
+++ romantoint/main.go
@@ -1,6 +1,22 @@
 package main
+
+import "fmt"
+var rommap= map[byte]int{
+'I'  : 1,
+'V'  : 5,
+'X'  : 10,
+'L'  : 50,
+'C'  : 100,
+'D'  : 500,
+'M'  : 1000,
+    }
 // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+func romanToInt(s string) int {
+return 3
+    
+}
+
 
 func main(){
-  
+  fmt.Print("map %v",rommap)
 }
