2d9b34c04c3654120491433f4aa8ce2623ca247f
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-01-31 18:10:57 +0530

    selection and bubble sort

--- sorting/main.go
+++ sorting/main.go
@@ -3,23 +3,43 @@ package main
 import "fmt"
 
 
-var myslice = []int{3, 8, 23, 73, 6, 2, 4, 1}
+var myslice = []int{-1, -1, 23, -1, 6, 0, 1, 1}
 
 func selectionSort(arr []int) {
-	
-	for i:=0;i<len(arr);i++{
-		selected:=arr[i]
-		for j:=i+1;j<len(arr);j++{
-          if(arr[j]<=selected){
-			arr[i],arr[j]=arr[j] ,arr[i]
+	n:=len(arr)
+	for i:=0;i<n-1;i++{
+		minindex:=i
+		for j:=i+1;j<n;j++{
+          if(arr[j]<arr[minindex]){
+			minindex=j
+			
 		  }
 		}
+       
+		arr[minindex],arr[i]=arr[i] ,arr[minindex]
+		
 	}
 
-	fmt.Print(arr,"sorted arrray")
+	fmt.Print(arr," selectionSort sorted arrray")
 	
+}
+
+
+func bubbleSort(arr[]int){
+	n:=len(arr)
+	for i:=0;i<n-1;i++{
+
+		for j:=i+1;j<n;j++{
+             if(arr[j]<arr[i]){
+				arr[j],arr[i]=arr[i],arr[j]
+			 }
+		}
+	}
+    fmt.Print(arr," bubbleSort sorted arrray")
+
 }
 
 func main() {
 selectionSort(myslice)
+bubbleSort(myslice)
 }
