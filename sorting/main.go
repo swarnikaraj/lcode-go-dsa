47df6fe6a9cee2a70966ac68705726c39c75f54d
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-02-01 23:50:19 +0530

    cyclic sort

--- sorting/main.go
+++ sorting/main.go
@@ -6,6 +6,7 @@ import "fmt"
 var myslice = []int{-1, -1, 23, -1, 6, 0, 1, 1}
 
 var myslice2 = []int{1, 2, 3, 4, 5, 6, 7, 8}
+var myslice3= []int{4, 2, 5, 1, 3}
 func selectionSort(arr []int) {
 	n:=len(arr)
 	for i:=0;i<n-1;i++{
@@ -45,7 +46,7 @@ func insertionsort(arr []int){
 
 n:=len(arr)
 for i:=0;i<=n-2;i++{
-	
+
 
 	for j:=i+1; j>0; j--{
 
@@ -64,8 +65,26 @@ for i:=0;i<=n-2;i++{
  fmt.Print(arr,"insertion sort")
 }
 
+
+func cyclicSort(arr []int){
+	i:=0
+	for i<len(arr){
+       value:=arr[i]
+		index:=value-1
+
+		if(i==index){
+			i++
+		}else{
+           arr[i], arr[index]=arr[index],arr[i]
+		}
+	}
+ fmt.Print(arr,"cyclic sort")
+}	
+
+
 func main() {
 // selectionSort(myslice)
 // bubbleSort(myslice2)
 insertionsort(myslice)
+cyclicSort(myslice3)
 }
