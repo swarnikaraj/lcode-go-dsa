ff85614394ce317291980b7c71df73a2fefd8f24
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-02-18 23:08:54 +0530

    setZeros

--- array/main.go
+++ array/main.go
@@ -726,6 +726,32 @@ func setZeroes(matrix [][]int)  {
      
 }
 
+func setZeroes2(matrix [][]int)  {
+   rows:=make([]int, len(matrix))
+   cols:=make([]int, len(matrix[0]))
+     for i:=0;i<len(matrix);i++{
+      for j:=0; j<len(matrix[0]);j++{
+
+        if matrix[i][j]==0{
+          rows[i]=1
+          cols[j]=1
+        }
+      }
+     }
+
+   
+       for i:=0;i<len(matrix);i++{
+       for j:=0; j<len(matrix[0]);j++{
+        if rows[i]==1 || cols[j]==1{
+         matrix[i][j]=0
+        }
+      }
+     }
+
+     fmt.Println(matrix)
+     
+}
+
 
 func main()  {
 // arr:=[]int{1 ,2, 8, 6 ,7 ,6 }
@@ -807,6 +833,7 @@ func main()  {
 // subarraySum(nums,k)
 
 mat :=[][]int{{1,1,1},{1,0,1},{1,1,1}}
-setZeroes(mat)
+// setZeroes(mat)
+setZeroes2(mat)
 }
 
