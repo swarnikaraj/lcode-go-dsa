716288e25ea596af7df789fdb1d8d199a5ea80a8
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-03-01 08:49:13 +0530

    rotate a string

--- strings/main.go
+++ strings/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func reverseWords(s string) string {
 	trimmed := strings.TrimSpace(s)
@@ -18,7 +21,37 @@ func reverseWords(s string) string {
 	newstr = newstr + strings.TrimSpace(arr[0])
 	return strings.TrimSpace(newstr)
 }
+
+func rotateString(s string, goal string) bool {
+ if len(s) != len(goal) {
+        return false
+    }
+    
+    if s == goal {
+        return true
+    }
+    finstr:=s
+    for i:=0;i<len(s);i++{
+        leftmost:=string(finstr[0])
+        fmt.Println(leftmost)
+
+       substr:=finstr[1:]
+       fmt.Println(substr)
+       joinstr:=substr + leftmost
+       fmt.Println(joinstr)
+       if joinstr == goal{
+           return true
+       }
+        finstr=joinstr
+        
+    }
+
+    return finstr==goal
+}
 func main() {
 	s := "a good   example"
 	reverseWords(s)
+s1:= "abcde"
+ goal := "cdeab"
+	rotateString(s1,goal)
 }
