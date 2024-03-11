b604e2a2c8fda892d2b2b42492cb41639a52520e
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-03-11 08:32:01 +0530

    counting bits and 1s in binary

--- bitwise/main.go
+++ bitwise/main.go
@@ -18,9 +18,41 @@ unique:=0
 
    return unique
 }
+// 338. Counting Bits
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 
+func count1(n int) int{
+    sum:=0
+    for n>0{
+        last:=n & 1
+        sum=sum+last
+        n=n>>1
+    }
+    return sum
+}
+func countBits(n int) []int {
+    ans:=make([]int,n+1)
+
+    i:=1
+
+    for i<n+1{
+       
+       total1:=count1(i)
+       ans[i]=total1
+    
+
+        i++
+    }
+   return ans 
+}
 func main(){
 	nums:=[]int{1,2,2,3,3,4,4,5,5}
 singleNumber(nums)
+countBits(9)
 }
