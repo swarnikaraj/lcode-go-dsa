8317c734977e6e4a283e8298c91fdf107ee2eea0
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-03-15 17:21:49 +0530

    stack implmented

--- stacks/main.go
+++ stacks/main.go
@@ -1,13 +1,32 @@
 package main
 
-
-type Stack struct{
-	
+type Stack struct {
+	size        int
+	CustomStack []int
+	ptr         int
 }
 
+func (st *Stack) addItem(n int) bool {
+	if st.size-1 == st.ptr {
+		return false
+	}
+	st.ptr++
+	st.CustomStack[st.ptr] = n
+	return true
+}
+func (st *Stack) removeItem() bool {
+	if st.ptr < 0 {
+		return false
+	}
+	st.ptr--
+	return true
 
-func main(){
+}
+func (st *Stack) peekItem() int {
 
+	return st.CustomStack[st.ptr]
+}
 
+func main() {
 
 }
